9f0b4618c2655e6cd71aa33d346cbcb4c7f0c3cd
NIU1603350 <53403172+OriolRoura@users.noreply.github.com>
2022-12-05 11:28:55 +0100

    inici

---
+++ src/test/java/steps/WomenDressSteps.java
@@ -0,0 +1,30 @@
+package steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class WomenDressSteps {
+
+    @Given("the user is in the index page")
+    public void theUserIsInTheIndexPage() {
+
+    }
+
+    @When("the user clikcs the products option")
+    public void theUserClicksTheProductsOpinion() {
+
+    }
+
+    @And("the user enters dress in the search bar")
+    public void theUserEntersDressInTheSearchBar() {
+
+    }
+
+    @Then("the dress list appears")
+    public void theDressListAppears() {
+
+    }
+
+}
---
+++ target/test-classes/features/womendress.feature
@@ -0,0 +1,8 @@
+Feature: WomenDress
+
+Scenario: Show Women Dress
+
+Given the user is in the index page
+When the user clikcs the products option
+And the user enters dress in the search bar
+Then the dress list appears
