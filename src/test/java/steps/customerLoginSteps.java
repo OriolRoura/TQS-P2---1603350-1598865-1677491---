23b2aa3d3a323c01e84b0c2e32693da44005665a
NIU1603350 <53403172+OriolRoura@users.noreply.github.com>
2022-12-19 20:30:42 +0100

    areglos

--- src/test/java/steps/customerLoginSteps.java
+++ src/test/java/steps/customerLoginSteps.java
@@ -28,7 +28,7 @@ public class customerLoginSteps {
     	driver.manage().window().maximize();
     }
  
- @When("^the user clikcs the ACCOUNT button")
+ @When("the user clikcs the ACCOUNT button")
     public void theUserClicksTheAccountButton() throws InterruptedException {
 	 	TimeUnit.SECONDS.sleep(1);
     	driver.findElement(By.id("ACCOUNT")).click();
@@ -38,37 +38,40 @@ public class customerLoginSteps {
     public void theUserClicksTheLoginButton(String Type) {
     	driver.findElement(By.partialLinkText(Type)).click();
     }
- 
- @Then("the user Logs in (.*)")
-    public void theUserLogIn(String Email, String Password){
-		driver.findElement(By.cssSelector("input[name='email']")).sendKeys(Email);
-        driver.findElement(By.cssSelector("input[name='password']")).sendKeys(Password);
-        driver.findElement(By.xpath("//*[@id=\"fadein\"]/div[4]/div/div[2]/div[2]/div/form/div[3]/button")).click();
-        String expectedUrl= driver.getCurrentUrl();
-        String actualUrl="https://phptravels.net/account/dashboard";
-        Assert.assertEquals(expectedUrl,actualUrl);
-    }
+ @And("^the user Logs in (.*),(.*)")
+ public void the_user_logs_in_agent_phptravels_com_demoagent(String Email, String Password) throws InterruptedException{
+	 TimeUnit.SECONDS.sleep(1);
+	 for (String window : driver.getWindowHandles()) {
+			driver.switchTo().window(window);
+		}
+	 driver.findElement(By.cssSelector("input[name='email']")).sendKeys(Email);
+     driver.findElement(By.cssSelector("input[name='password']")).sendKeys(Password);
+     driver.findElement(By.cssSelector("button[type='submit']")).click();
+     String expectedUrl= driver.getCurrentUrl();
+     String actualUrl="https://phptravels.net/account/dashboard";
+     Assert.assertTrue(expectedUrl.equals(actualUrl));
+ }	
    
- @When("the user clikcs the MyProfile button")
+ @And("the user clikcs the MyProfile button")
     public void theUserClicksTheMyProfileButton() {
     	driver.findElement(By.xpath("//*[@id=\"fadein\"]/div[4]/div/div[3]/ul/li[4]/a")).click();
     	
     }
     
- @Then("we compare the account email to the one we submited")
-    public void WeCompareTheAccountEmailToTheOneWeSubmited() {
-    	String expectedUser = "Demo";
-    	String User = driver.findElement(By.xpath("/html/body/div[4]/div/div[2]/div/div[2]/h4/strong")).getText();
-    	Assert.assertEquals(expectedUser,User);
+ @Then("^we compare the (.*) email to the one we submited")
+    public void WeCompareTheAccountEmailToTheOneWeSubmited(String user) {
+    	String expectedUser = user;
+    	String User = driver.findElement(By.xpath("//*[@id=\"fadein\"]/div[4]/div/div[2]/div/div[2]/h4/strong")).getText();
+    	Assert.assertTrue(expectedUser.contains(User));
     	
    }
  @Then("we compare the account email to the one we submited supplier")
  public void WeCompareTheAccountEmailToTheOneWeSubmitedSupplier() {
-	driver.findElement(By.xpath("/html/body/nav/div/div/div/div[3]/button/i")).click()
-	driver.findElement(By.xpath("/html/body/nav/div/div/div/div[3]/ul/li[1]/a/div")).click()
+	driver.findElement(By.xpath("/html/body/nav/div/div/div/div[3]/button/i")).click();
+	driver.findElement(By.xpath("/html/body/nav/div/div/div/div[3]/ul/li[1]/a/div")).click();
  	String expectedUser = "Demo";
  	String User = driver.findElement(By.xpath("//*[@id=\"layoutDrawer_content\"]/main/div/form/div/div/div/div/div[3]/div/div/div[2]/div/input")).getText();
- 	Assert.assertEquals(expectedUser,User);
+ 	Assert.assertTrue(expectedUser.contains(User));
  	
 }
  
@@ -77,13 +80,13 @@ public class customerLoginSteps {
 		driver.close();
 	}
  
- @Then("^the user Logs in wrong (.*)(.*)")
+ @Then("^the user Logs in wrong (.*),(.*)")
  public void theUserLogInWrong(String Email, String Password){
-		driver.findElement(By.cssSelector("input[name='email']")).sendKeys(Email);
-     driver.findElement(By.cssSelector("input[name='password']")).sendKeys(Password);
+	 driver.findElement(By.name("email")).sendKeys(Email);
+     driver.findElement(By.name("password")).sendKeys(Password);
      driver.findElement(By.xpath("//*[@id=\"fadein\"]/div[4]/div/div[2]/div[2]/div/form/div[3]/button")).click();
      String expectedUrl= driver.getCurrentUrl();
-     String actualUrl="https://phptravels.net/login/failed;
+     String actualUrl="https://phptravels.net/login/failed";
      Assert.assertEquals(expectedUrl,actualUrl);
  	
 }
--- target/test-classes/features/customerLogin.feature
+++ target/test-classes/features/customerLogin.feature
@@ -1,29 +1,41 @@
 Feature: Login
+
 @tag1
-Scenario Outline: Make Login
+Scenario Outline: Make Login Client/Agent
 
 Given the user is in the logIn page
 When the user clikcs the ACCOUNT button
 And clicks the Customer Login button <Type>
-Then the user Logs in <Email> <Password>
-When the user clikcs the MyProfile button 
-Then we compare the account email to the one we submited
+And the user Logs in <Email>,<Password>
+And the user clikcs the MyProfile button 
+Then we compare the <user> email to the one we submited
 And close
 
 Examples: 
-| Type | Email | Password |
-| Customer Login | user@phptravels.com | demouser |
-| Agents Login | agent@phptravels.com | demoagent |
-| Supplier Login | supplier@phptravels.com | demosupplier |
-
-
+| Type | Email | Password | user |
+| Customer Login | user@phptravels.com | demouser | Елена |
+| Agents Login | agent@phptravels.com | demoagent | Demo |
 
 @tag2
+Scenario Outline: Make Login Supplier
+Given the user is in the logIn page
+When the user clikcs the ACCOUNT button
+And clicks the Customer Login button <Type>
+And the user Logs in <Email>,<Password>
+And the user clikcs the MyProfile button 
+Then we compare the account email to the one we submited supplier
+And close
+
+Examples:
+| Type | Email | Password |
+| Supplier Login | supplier@phptravels.com | demosupplier |
+
+@tag3
 Scenario Outline: Failed Login
 
-Given the user is in the logIN page
-When the user clicks the ACCOUNT button
-And clicks the Customer button <Type>
+Given the user is in the logIn page
+When the user clikcs the ACCOUNT button
+And clicks the Customer Login button <Type>
 Then the user Logs in wrong <Email> <Password>
 And close
 
